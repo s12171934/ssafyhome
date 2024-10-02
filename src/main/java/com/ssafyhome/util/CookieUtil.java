1a7ef80cc6c39f7734bb7313382e7345243998ee
s12171934 <ssong99999@naver.com>
2024-10-02 09:17:15 +0900

    FEATURE: jwt, cookie util

--- src/main/java/com/ssafyhome/util/CookieUtil.java
+++ src/main/java/com/ssafyhome/util/CookieUtil.java
@@ -1,7 +1,26 @@
 package com.ssafyhome.util;
 
+import jakarta.servlet.http.Cookie;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CookieUtil {
+
+  public Cookie createCookie(String key, String value) {
+
+    Cookie cookie = new Cookie(key, value);
+    cookie.setPath("/");
+    cookie.setHttpOnly(true);
+    cookie.setSecure(true);
+    cookie.setMaxAge(24 * 60 * 60);
+    return cookie;
+  }
+
+  public Cookie deleteCookie(String key) {
+
+    Cookie cookie = new Cookie(key, null);
+    cookie.setPath("/");
+    cookie.setMaxAge(0);
+    return cookie;
+  }
 }
--- src/main/java/com/ssafyhome/util/JWTUtil.java
+++ src/main/java/com/ssafyhome/util/JWTUtil.java
@@ -1,7 +1,65 @@
 package com.ssafyhome.util;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 @Component
 public class JWTUtil {
+
+  private SecretKey secretKey;
+
+  public JWTUtil(
+      @Value("${jwt.secret}")
+      String secret
+  ) {
+
+    this.secretKey = new SecretKeySpec(
+        secret.getBytes(StandardCharsets.UTF_8),
+        Jwts.SIG.HS256.key().build().getAlgorithm()
+    );
+  }
+
+  public Claims parseToken(String token) {
+
+    return Jwts.parser()
+        .verifyWith(secretKey)
+        .build()
+        .parseSignedClaims(token)
+        .getPayload();
+  }
+
+  public String getKey(String token, String key) {
+
+    return parseToken(token).get(key, String.class);
+  }
+
+  public Boolean isExpired(String token) {
+
+    try {
+      Date expiration = parseToken(token).getExpiration();
+      return expiration.before(new Date());
+    } catch (ExpiredJwtException e) {
+      return true;
+    }
+  }
+
+  public String createJWT(String category, String userId, String userEmail, Long expiration) {
+
+    return Jwts.builder()
+        .claim("category", category)
+        .claim("userId", userId)
+        .claim("userEmail", userEmail)
+        .issuedAt(new Date(System.currentTimeMillis()))
+        .expiration(new Date(System.currentTimeMillis() + expiration))
+        .signWith(secretKey)
+        .compact();
+  }
 }
