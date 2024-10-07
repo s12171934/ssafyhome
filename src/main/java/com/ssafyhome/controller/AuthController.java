c7655c68758938df8a752cc63dbc8094bdb60e17
s12171934 <ssong99999@naver.com>
2024-10-07 16:07:56 +0900

    FEATURE: AuthController

--- src/main/java/com/ssafyhome/controller/AuthController.java
+++ src/main/java/com/ssafyhome/controller/AuthController.java
@@ -1,9 +1,26 @@
 package com.ssafyhome.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ssafyhome.model.service.JWTService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
+  private final JWTService jwtService;
+
+  public AuthController(JWTService jwtService) {
+
+    this.jwtService = jwtService;
+  }
+
+  @PostMapping("/reissue")
+  public ResponseEntity<?> reissue(
+      @CookieValue(value = "refreshToken", defaultValue = "no_refresh_token")
+      String refreshToken
+  ) {
+
+    return jwtService.reissueRefreshToken(refreshToken);
+  }
 }
