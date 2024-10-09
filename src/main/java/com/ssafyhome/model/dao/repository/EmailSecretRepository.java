fc00d525df995ad5cae4b217fd84ded7fc9a08f1
s12171934 <ssong99999@naver.com>
2024-10-09 23:37:27 +0900

    [FEAT] secretUtil to make random String secret

--- src/main/java/com/ssafyhome/model/dao/repository/EmailSecretRepository.java
+++ src/main/java/com/ssafyhome/model/dao/repository/EmailSecretRepository.java
@@ -3,5 +3,5 @@ package com.ssafyhome.model.dao.repository;
 import com.ssafyhome.model.dto.entity.redis.EmailSecretEntity;
 import org.springframework.data.repository.CrudRepository;
 
-public interface EmailSecretRepository extends CrudRepository<String, EmailSecretEntity> {
+public interface EmailSecretRepository extends CrudRepository<EmailSecretEntity, String> {
 }
