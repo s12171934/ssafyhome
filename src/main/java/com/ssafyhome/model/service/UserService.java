0f9811a8dad68d751bd7974e6353b8a2e93c7801
s12171934 <ssong99999@naver.com>
2024-10-10 15:29:01 +0900

    [FEAT] implement user service

--- src/main/java/com/ssafyhome/model/service/UserService.java
+++ src/main/java/com/ssafyhome/model/service/UserService.java
@@ -1,10 +1,26 @@
 package com.ssafyhome.model.service;
 
+import com.ssafyhome.model.dto.FindUserDto;
+import com.ssafyhome.model.dto.PasswordDto;
 import com.ssafyhome.model.dto.UserDto;
+import com.ssafyhome.model.dto.UserSearchDto;
+import com.ssafyhome.model.dto.entity.mysql.UserEntity;
+import com.ssafyhome.model.dto.entity.redis.EmailSecretEntity;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public interface UserService {
 
   ResponseEntity<?> register(UserDto userDto);
   ResponseEntity<?> sendEmail(String email);
+  ResponseEntity<?> findUserId(FindUserDto findUserDto);
+  ResponseEntity<?> findPassword(FindUserDto findUserDto);
+  ResponseEntity<?> changePassword(String userSeq, PasswordDto passwordDto);
+  ResponseEntity<?> checkEmailSecret(EmailSecretEntity emailSecretEntity);
+  ResponseEntity<?> checkIdDuplicate(String id);
+  ResponseEntity<?> updateUser(UserEntity userEntity);
+  ResponseEntity<?> deleteUser(String userSeq);
+  ResponseEntity<List<UserDto>> getUserList(UserSearchDto userSearchDto);
+  ResponseEntity<UserDto> getUserInfo(String userSeq);
 }
