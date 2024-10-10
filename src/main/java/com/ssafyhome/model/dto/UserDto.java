be54486fe45bafe9b98aa6c7b42cffd660b2bbb4
s12171934 <ssong99999@naver.com>
2024-10-10 15:29:32 +0900

    [FEAT] add Dto field

--- src/main/java/com/ssafyhome/model/dto/UserDto.java
+++ src/main/java/com/ssafyhome/model/dto/UserDto.java
@@ -1,10 +1,13 @@
 package com.ssafyhome.model.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class UserDto {
 
+  private long userSeq;
   private String userId;
   private String userPassword;
   private String userPasswordConfirm;
